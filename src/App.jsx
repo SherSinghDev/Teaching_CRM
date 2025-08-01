9b032b55d26d9e74c8906cbbf783eeb5886a053e
SherSinghDev <shersingh741730@gmail.com>
2025-08-01 12:00:31 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Dashboard from "./components/pages/Dashboard"
+import Report from "./components/pages/Report"
+
+function App() {
+  return (
+    <>
+      <Dashboard/>
+      {/* <Report/> */}
+    </>
+  )
+}
+
+export default App
